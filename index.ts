fe218521beb1698ba978be5bb188f51918adf559
JeroenBos <jer123bos@gmail.com>
2019-10-13 21:58:18 +0200

    Export more via the index

--- index.ts
+++ index.ts
@@ -1,18 +1,16 @@
 import container from './IoC/container'; // the empty container
 
 export { container };
-export { identifiers } from './IoC/keys';
-export { AbstractCommandManager, CommandManagerProps, CommandManagerState } from './commands/abstractCommandManager';
-export { BaseState, IComponent } from './base.interfaces';
+export { IChangePropagator, BaseProps, AppId, UNINITIALIZED_ID, ICommandManager, CommandManagerId, BaseState, IComponent } from './base.interfaces';
+export { BaseComponent, SimpleStateInfo, StateInfoLocalHelper } from './base.component';
 export { Http } from './changesPropagator/http';
 export { ChangesPropagator, IResponse } from "./changesPropagator/ChangesPropagator";
-export { IChangePropagator, BaseProps, AppId, UNINITIALIZED_ID, ICommandManager, CommandManagerId } from './base.interfaces';
-export { AbstractProvider } from 'jbsnorro/IoC/AbstractProvider';
-export { IProvider } from 'jbsnorro/IoC/IProvider';
-export { IResettableProvider } from 'jbsnorro/IoC/IResettableProvider';
-export { ResettableContainer } from 'jbsnorro/IoC/ResettableContainer';
-export { BaseComponent, SimpleStateInfo, StateInfoLocalHelper } from './base.component';
-export { TempIdProvider } from './tempIdProvider';
-export { defaultTempIdProviderProvider, mockHttpProvider, defaultServerProvider, defaultCommandManagerProvider, ITestResponse } from './IoC/defaults';
+export { AbstractCommandManager, CommandManagerProps, CommandManagerState } from './commands/abstractCommandManager';
 export { CommandInstruction } from './commands/commandInstruction';
+export { CommandViewModel } from './commands/commands';
 export { PromiseFactory } from './core';
+export { defaultTempIdProviderProvider, mockHttpProvider, defaultServerProvider, defaultCommandManagerProvider, ITestResponse, MockCommandInstruction } from './IoC/defaults';
+export { identifiers, TServices } from './IoC/keys';
+export { TempIdProvider } from './tempIdProvider';
+
+export { ReactWrapper, wraps } from './tests/enzyme.wrapper';
