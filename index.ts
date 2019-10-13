e8993c7e31d540900d02aac73b7f61c5244c849e
JeroenBos <jer123bos@gmail.com>
2019-10-13 20:30:50 +0200

    Implement index.ts (because I keep getting 'Please contact the package author to advise them to use --declaration --outDir.' even though I do)

--- index.ts
+++ index.ts
@@ -1,17 +1,18 @@
-// TS:
+import container from './IoC/container'; // the empty container
 
-// export { OptionalToMissing, IsExact } from './typeHelper';
-
-// JS:
-
-// import {
-//     assert,
-// } from './typeHelper';
-
-export {
-    // assert,
-};
-
-export default {
-    // assert
-};
+export { container };
+export { identifiers } from './IoC/keys';
+export { AbstractCommandManager, CommandManagerProps, CommandManagerState } from './commands/abstractCommandManager';
+export { BaseState, IComponent } from './base.interfaces';
+export { Http } from './changesPropagator/http';
+export { ChangesPropagator, IResponse } from "./changesPropagator/ChangesPropagator";
+export { IChangePropagator, BaseProps, AppId, UNINITIALIZED_ID, ICommandManager, CommandManagerId } from './base.interfaces';
+export { AbstractProvider } from 'jbsnorro/IoC/AbstractProvider';
+export { IProvider } from 'jbsnorro/IoC/IProvider';
+export { IResettableProvider } from 'jbsnorro/IoC/IResettableProvider';
+export { ResettableContainer } from 'jbsnorro/IoC/ResettableContainer';
+export { BaseComponent, SimpleStateInfo, StateInfoLocalHelper } from './base.component';
+export { TempIdProvider } from './tempIdProvider';
+export { defaultTempIdProviderProvider, mockHttpProvider, defaultServerProvider, defaultCommandManagerProvider, ITestResponse } from './IoC/defaults';
+export { CommandInstruction } from './commands/commandInstruction';
+export { PromiseFactory } from './core';
