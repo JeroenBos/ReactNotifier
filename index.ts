573cfa20518a5df6f4e5ba36905b0f87d529cd0c
JeroenBos <jer123bos@gmail.com>
2019-10-21 15:32:08 +0200

    add comments

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import container from './IoC/container'; // the empty container
 
+// you cannot export anything from the './tests' directory, because its types aren't published (by default)
 export { container };
 export { TypedContainer } from './IoC/container';
 export { IChangePropagator, BaseProps, AppId, UNINITIALIZED_ID, ICommandManager, CommandManagerId, BaseState, IComponent } from './base.interfaces';
@@ -14,5 +15,4 @@ export { PromiseFactory } from './core';
 export { defaultTempIdProviderProvider, mockHttpProvider, defaultServerProvider, defaultCommandManagerProvider, ITestResponse, MockCommandInstruction, ITestChange } from './IoC/defaults';
 export { identifiers, TServices } from './IoC/keys';
 export { TempIdProvider } from './tempIdProvider';
-
 export { ReactWrapper, wraps } from './enzyme.wrapper';
