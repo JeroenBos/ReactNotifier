ed40b5e93df4b7cc47019fa58d9a0469038f392e
JeroenBos <jer123bos@gmail.com>
2019-10-25 23:53:38 +0200

    Relax assertion in container.resetAll()

--- IoC/container.ts
+++ IoC/container.ts
@@ -24,11 +24,6 @@ export class TypedContainer extends ResettableContainer<TServices> implements TS
     get typesystem(): TypeSystem<any> {
         return this.getProvider(identifiers.typesystem).provide();
     }
-
-    resetAll() {
-        assert(this.isBound(identifiers.rootIds), 'rootIds must be bound before calling resetAll');
-        super.resetAll();
-    }
 }
 
 export default new TypedContainer(identifiers);
